32b81048a0fe7d9cab873d612e6482645076f4f5
bitgold8 <maelokvevos@gmail.com>
2025-09-12 17:47:04 +0100

    Test genesis premine spend

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -60,15 +60,8 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
 }
 
 /**
- * Build the genesis block. Note that the output of its generation
- * transaction cannot be spent since it did not originally exist in the
- * database.
- *
- * CBlock(hash=000000000019d6, ver=1, hashPrevBlock=00000000000000, hashMerkleRoot=4a5e1e, nTime=1231006505, nBits=1d00ffff, nNonce=2083236893, vtx=1)
- *   CTransaction(hash=4a5e1e, ver=1, vin.size=1, vout.size=1, nLockTime=0)
- *     CTxIn(COutPoint(000000, -1), coinbase 04ffff001d0104455468652054696d65732030332f4a616e2f32303339204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73)
- *     CTxOut(nValue=50.00000000, scriptPubKey=0x5F1DF16B2B704C8A578D0B)
- *   vMerkleTree: 4a5e1e
+ * Build the genesis block, minting the initial 3,000,000 coins and exposing
+ * a spendable output.
  */
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
@@ -169,6 +162,7 @@ public:
         genesis = CreateGenesisBlock(genesis_timestamp, genesis_script,
                                      1704067200, 1106766, 0x1e0ffff0, 1,
                                      3'000'000 * COIN);
+        assert(genesis.vtx[0]->vout[0].nValue == 3'000'000 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
         consensus.defaultAssumeValid =
             uint256{"0000030e31ae394ca69551a5ed5a321d1175e043efae4afacdf09351a0b8a83c"};
